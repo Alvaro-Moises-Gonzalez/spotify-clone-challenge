fcc7d5b7b1e1a3c082eb1a89e59aee184de5c50e
Francisco Gonzalez <fg@altusforge.com>
2022-06-16 14:14:34 -0500

    Add fa icons and prettier config

--- src/main.js
+++ src/main.js
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(far, fab, fas)
+
+dom.watch()
+
 createApp(App).use(router).mount('#app')
--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <div class="user-info">
-      <img src="../assets/placeholder.jpg" alt="foto de perfil"/>
+      <img src="@/assets/placeholder.jpg" alt="foto de perfil" />
       <button>Inicio</button>
       <button>Buscar</button>
       <button>Biblioteca</button>
@@ -14,25 +14,25 @@
 </template>
 
 <style scoped>
-  .wrapper{
-    display: flex;
-  }
-  .user-info{
-    width: auto;
-    height: 700px;
-    display: flex;
-    flex-direction: column;
-    background-color: red;
-  }
-  .user-info img{
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    margin: 40px;
-  }
-  .categories{
-    width: 100%;
-    height: 350px;
-    background-color: blue;
-  }
+.wrapper {
+  display: flex;
+}
+.user-info {
+  width: auto;
+  height: 700px;
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+}
+.user-info img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin: 40px;
+}
+.categories {
+  width: 100%;
+  height: 350px;
+  background-color: blue;
+}
 </style>
