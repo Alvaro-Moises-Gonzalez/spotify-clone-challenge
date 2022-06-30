276d2624b14d924d592054aa6c1b3c37d1922304
Álvaro González Quintero <97643699+Alvaro-Moises-Gonzalez@users.noreply.github.com>
2022-06-30 15:07:01 -0500

    4.6.5 created MusicPlayer, layout fixes on UserInfo component

---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="wrapper">
+    <button class="backward" @click="prevTrack">
+      <i class="fas fa-backward"></i>
+    </button>
+    <button
+      v-if="isPlaying"
+      class="media"
+      :class="{ active: isPlaying }"
+      @click="togglePlay(); play();"
+      >
+      <i class="fas fa-play"></i>
+    </button>
+    <button
+      v-else
+      class="media"
+      @click="pause"
+    >
+      <i class="fas fa-pause"></i>
+    </button>
+    <button class="foward" @click="nextTrack">
+      <i class="fas fa-forward"></i>
+    </button>
+    <p class="name">{{ trackName }}</p>
+    <p>{{ msToMinutes(0) }}</p>
+    <input type="range" min="0" max="100" value="25" />
+    <p>{{ msToMinutes(parseInt(duration)) }}</p>
+  </div>
+</template>
+
+<script>
+import { playerEndpoints } from '@/api/endpoints'
+export default {
+  data() {
+    return {
+      isPlaying: true,
+      trackName: '',
+      artistName: '',
+      duration: '0'
+    }
+  },
+  props: {
+    play: {
+      type: Function,
+      default () {
+        return console.log('default function')
+      }
+    }
+  },
+  methods: {
+    togglePlay() {
+      this.isPlaying = !this.isPlaying
+    },
+    msToMinutes(ms) {
+      var minutes = Math.floor(ms / 60000)
+      var seconds = ((ms % 60000) / 1000).toFixed(0)
+      return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+    },
+    async pause() {
+      const config = {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`
+        }
+      }
+      await fetch(
+        playerEndpoints.pausePlayback(localStorage.getItem('DEVICE_ID')),
+        config
+      )
+      this.togglePlay()
+    },
+    async prevTrack() {
+      await fetch(playerEndpoints.skiptoPrevius, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`
+        }
+      })
+      if(this.isPlaying !== false) {
+        this.togglePlay()
+      }
+
+    },
+    async nextTrack() {
+      const config = {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`,
+          'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+          clear_preloaded: 'true',
+          play: 'false'
+        })
+      }
+      await fetch(
+        playerEndpoints.skipToNext(localStorage.getItem('DEVICE_ID')),
+        config
+      )
+      if(this.isPlaying !== false) {
+        this.togglePlay()
+      }
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  background: rgba(0, 0, 0, 0.3);
+  height: 100px;
+  width: 250px;
+  place-self: center;
+  margin: 10px;
+  border-radius: 15px;
+  display: grid;
+  grid-template: auto / 0.5fr 1fr 0.5fr;
+  color: white;
+}
+
+.media {
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  place-self: center;
+  margin: 5px;
+  background: rgba(0, 0, 0, 0.3);
+  border: none;
+  color: white;
+}
+
+.media:hover {
+  outline: 1px solid rgb(68, 255, 47);
+  color: rgb(60, 255, 47);
+}
+.foward {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  justify-self: start;
+  align-self: center;
+  background: rgba(0, 0, 0, 0.3);
+  border: none;
+  color: white;
+}
+.backward {
+  justify-self: end;
+  align-self: center;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  background: rgba(0, 0, 0, 0.3);
+  border: none;
+  color: white;
+}
+
+.backward:hover {
+  outline: 1px solid rgb(68, 255, 47);
+  color: rgb(68, 255, 47);
+  cursor: pointer;
+}
+
+.foward:hover {
+  outline: 1px solid rgb(68, 255, 47);
+  color: rgb(68, 255, 47);
+  cursor: pointer;
+}
+
+.active {
+  color: rgb(60, 255, 47);
+  outline: 1px solid rgb(60, 2550, 47);
+}
+
+.name {
+  grid-column: span 3;
+}
+
+p {
+  place-self: center;
+}
+
+input {
+  background: rgba(0, 0, 0, 0.2);
+}
+
+::-moz-range-progress {
+  background: rgb(68, 255, 47);
+}
+
+::-moz-range-track {
+  background-color: rgba(0, 0, 0, 0.3);
+}
+
+::-moz-range-thumb {
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: rgb(27, 254, 2);
+  border: none;
+}
+</style>
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,20 +1,45 @@
 <template>
-   <div class="user-info">
-            <img :src="userInfo.images[0].url" alt='foto de perfil' />
-            <p>{{ userInfo.display_name }}</p>
-            <p class="email">{{ userInfo.email }}</p>
-            <p class="country">Country: {{ userInfo.country }}</p>
-            <router-link :to="{ path: '/home' }"><button><span><i class="fa fa-home-alt icon"></i></span>Home</button></router-link>
-            <router-link :to="{ path: '/search'}"><button><span><i class="fas fa-search icon"></i></span>Search</button></router-link>
-            <router-link :to="{ path: '/playlist'}"><button><span><i class="fas fa-book ico"></i></span>My Playlists</button></router-link>
-            <button class="logout-btn" @click=logout >Logout</button>
-        </div>
+  <div class="user-info">
+    <img :src="userInfo.images[0].url" alt="foto de perfil" />
+    <p>{{ userInfo.display_name }}</p>
+    <p class="email">{{ userInfo.email }}</p>
+    <p class="country">Country: {{ userInfo.country }}</p>
+    <router-link :to="{ path: '/home' }"
+      ><button>
+        <span><i class="fa fa-home-alt icon"></i></span>Home
+      </button></router-link
+    >
+    <router-link :to="{ path: '/search' }"
+      ><button>
+        <span><i class="fas fa-search icon"></i></span>Search
+      </button></router-link
+    >
+    <router-link :to="{ path: '/playlist' }"
+      ><button>
+        <span><i class="fas fa-book ico"></i></span>My Playlists
+      </button></router-link
+    >
+    <button class="logout-btn" @click="logout">Logout</button>
+    <music-player />
+  </div>
 </template>
 
 <script>
+import MusicPlayer from '@/components/MusicPlayer.vue'
+import { playerEndpoints } from '@/api/endpoints'
 export default {
   methods: {
-    logout () {
+    async logout() {
+      const config = {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`
+        }
+      }
+      await fetch(
+        playerEndpoints.pausePlayback(localStorage.getItem('DEVICE_ID')),
+        config
+      )
       localStorage.removeItem('CODE')
       localStorage.removeItem('ACCESS_TOKEN')
       localStorage.removeItem('EXPIRES')
@@ -25,7 +50,7 @@ export default {
   props: {
     userInfo: {
       type: Object,
-      default (rawProps) {
+      default(rawProps) {
         return {
           images: ['@/assets/placeholder.jpg'],
           email: '',
@@ -34,33 +59,36 @@ export default {
         }
       }
     }
+  },
+  components: {
+    MusicPlayer
   }
 }
 </script>
 
 <style scoped>
 .user-info {
-    width: auto;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    background: linear-gradient(rgba(0,0,0, 0.8), rgba(0,0,0, 0.2));
-    position: relative;
+  width: auto;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.2));
+  position: relative;
 }
 .user-info img {
-    width: 220px;
-    height: 230px;
-    border-radius: 50%;
-    margin: 20px;
-    background-size: contain;
+  width: 220px;
+  height: 230px;
+  border-radius: 50%;
+  margin: 20px;
+  background-size: contain;
 }
-.user-info p{
+.user-info p {
   color: white;
   text-align: center;
   font-size: 3rem;
   margin: 10px 0px;
 }
-.user-info .email{
+.user-info .email {
   font-size: 1.5rem;
   margin: 5px 0px 5px 0px;
 }
@@ -82,18 +110,15 @@ button {
   border-bottom: 1px solid transparent;
   margin-right: 10px;
 }
-button span{
+button span {
   margin-right: 10px;
 }
-button:hover{
+button:hover {
   color: aqua;
   border-bottom: 1px solid aqua;
-
 }
-.logout-btn{
-  position: absolute;
-  bottom: 15px;
-  background: rgba(0,0,0, 0.3);
+.logout-btn {
+  background: rgba(0, 0, 0, 0.3);
   width: 100px;
   height: 60px;
   border-radius: 15px;
@@ -101,7 +126,7 @@ button:hover{
   color: white;
   border: 1px solid transparent;
 }
-.logout-btn:hover{
+.logout-btn:hover {
   border: 1px solid aqua;
 }
 </style>
